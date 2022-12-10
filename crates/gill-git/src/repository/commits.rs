ad4cb82b72fbe6e45717075eb1da518a128dfbab
Paul Delafosse <paul.delafosse@protonmail.com>
2022-12-10 12:40:59 +0100

    feat: add building blocks for apu commits

--- crates/gill-git/src/repository/commits.rs
+++ crates/gill-git/src/repository/commits.rs
@@ -2,6 +2,12 @@ use crate::repository::REPO_DIR;
 use git_repository::Commit;
 use std::path::PathBuf;
 
+pub fn by_sha<'a>(namespace: &'a str, name: &'a str, sha: &'a str) -> anyhow::Result<OwnedCommit> {
+    let path = PathBuf::from(REPO_DIR).join(namespace).join(name);
+    let repo = git_repository::open(path)?;
+    imp::find_commit(&repo, sha)
+}
+
 pub fn history<'a>(namespace: &'a str, name: &'a str) -> anyhow::Result<Vec<OwnedCommit>> {
     let path = PathBuf::from(REPO_DIR).join(namespace).join(name);
     let repo = git_repository::open(path)?;
@@ -15,7 +21,8 @@ pub struct OwnedCommit {
     pub body: Option<String>,
     pub author: String,
     pub email: String,
-    pub time: u32,
+    pub created_at: u32,
+    pub authored_at: u32,
 }
 
 impl TryFrom<Commit<'_>> for OwnedCommit {
@@ -26,8 +33,9 @@ impl TryFrom<Commit<'_>> for OwnedCommit {
         let id = commit.id.to_string();
         let summary = message_ref.summary().to_string();
         let body = message_ref.body.map(ToString::to_string);
-        let time = commit.time()?.seconds();
+        let created_at = commit.time()?.seconds();
         let signature_ref = commit.author()?;
+        let authored_at = signature_ref.time.seconds();
         let author = signature_ref.name.to_string();
         let email = signature_ref.email.to_string();
 
@@ -37,7 +45,8 @@ impl TryFrom<Commit<'_>> for OwnedCommit {
             body,
             author,
             email,
-            time,
+            created_at,
+            authored_at,
         })
     }
 }
@@ -45,7 +54,13 @@ impl TryFrom<Commit<'_>> for OwnedCommit {
 mod imp {
     use crate::repository::commits::OwnedCommit;
     use anyhow::Result;
-    use git_repository::Repository;
+    use git_repository::{ObjectId, Repository};
+
+    pub fn find_commit(repo: &Repository, sha: &str) -> Result<OwnedCommit> {
+        let object_id = ObjectId::from_hex(sha.as_bytes())?;
+        let commit = repo.find_object(object_id)?.try_into_commit()?;
+        OwnedCommit::try_from(commit)
+    }
 
     pub fn list_commits(repo: &Repository) -> Result<Vec<OwnedCommit>> {
         let head = repo.head_commit()?;
@@ -62,17 +77,27 @@ mod imp {
 
 #[cfg(test)]
 mod test {
-    use crate::repository::commits::imp::list_commits;
+    use crate::repository::commits::imp::{find_commit, list_commits};
 
     #[test]
     // TODO: Make some actual assertion here
-    // need sealed test to setup a fake repository
+    //  need sealed test to setup a fake repository
     fn list_repository_commits() -> anyhow::Result<()> {
         let repo = git_repository::open("/home/okno/Code/gill")?;
         let commits = list_commits(&repo)?;
         for x in commits {
             println!("{:#?}", x);
         }
+        Ok(())
+    }
+
+    #[test]
+    // TODO: Make some actual assertion here
+    //  need sealed test to setup a fake repository
+    fn find_commit_ok() -> anyhow::Result<()> {
+        let repo = git_repository::open("/home/okno/Code/gill")?;
+        let commit = find_commit(&repo, "4dfecc6603e9332279a83f045b7afbd4c6f5816e");
+        assert!(commit.is_ok());
         Ok(())
     }
 }
