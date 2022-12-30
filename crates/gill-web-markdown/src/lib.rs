910c9d32cf9ae5d476611d38135a8e8be32deb36
Paul Delafosse <paul.delafosse@protonmail.com>
2022-12-30 15:44:30 +0100

    feat: add markdown preview and wasm module

---
+++ crates/gill-web-markdown/src/lib.rs
@@ -0,0 +1,14 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn render_markdown(markdown_input: String, owner: String, repository: String) -> String {
+    gill_markdown::render(&markdown_input, &owner, &repository)
+}
